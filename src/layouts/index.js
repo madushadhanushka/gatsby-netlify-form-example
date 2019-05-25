33cef20088e312cbe9bcac8346bed8bd1e89de45
dhanushka <madushandhanushka@gmail.com>
2019-05-26 00:45:58 +0530

    initial commit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -49,7 +49,6 @@ export default class Template extends React.Component {
         <div
           style={{
             margin: `0 auto`,
-            maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
-
+var diff = require('diff')
+import { Diff2Html } from 'diff2html'
+import 'react-gh-like-diff/lib/diff2html.min.css';
 export default class Index extends React.Component {
+  getDiff() {
+    var changesOth = diff.createPatch("Changes", "slkdjflks sfd", "kdkdkdkd sfd")
+    var diffHtml = Diff2Html.getPrettyHtml(
+      changesOth,
+      {inputFormat: 'diff', showFiles: true, matching: 'lines', outputFormat: 'side-by-side'}
+    );
+    document.getElementById("diff_content").innerHTML = diffHtml;
+    console.log("done", changesOth)
+  };
   render() {
     return (
       <div>
@@ -14,7 +25,11 @@ export default class Index extends React.Component {
           <li><Link to="/contact/">Basic contact form</Link></li>
           <li><Link to="/file-upload/">Form with file upload</Link></li>
           <li><Link to="/recaptcha/">Form with reCAPTCHA 2</Link></li>
+          <textarea name="fileOne" />
+          <textarea name="fileTwo" />
+          <button type="submit" onClick={this.getDiff}>Send</button>
         </ul>
+        <div id="diff_content"></div>
       </div>
     );
   }
